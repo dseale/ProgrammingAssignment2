20889375bac622c3cb2a9f810294b8277f514cda
dseale <drew@savantsys.com>
2017-09-22 15:05:55 -0500

    Initial function upload
    enter the commit message for your changes. Lines starting
    Initial upload with base functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## Creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     
+     i <- NULL
+     
+     set <- function(y) {
+          x <<- y
+          i <<- NULL
+     }
+     
+     get = function() { x }
+     
+     setInverse <- function(inverse) { i <<- inverse }
+     
+     getInverse <- function() { i }
+     
+     list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
+     
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix 
+## If the inverse has already been calculated (and the matrix has not changed), this retrieves the computed inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     
+     i <- x$getInverse()
+     
+     if(!is.null(i)) {
+          message("getting cached data")
+          return(i)
+     }
+     
+     val <- x$get()
+     i <- solve(val, ...)
+     
+     
+     x$setInverse(i)
+     i
 }
